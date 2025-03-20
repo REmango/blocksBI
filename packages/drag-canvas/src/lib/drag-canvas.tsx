8213f4009c4a45b106b660bf4bf32e75a6b74344
lizheng <lizheng@tencent.com>
2025-03-20 23:57:05 +0800

    feat: 添加吸附

--- packages/drag-canvas/src/lib/drag-canvas.tsx
+++ packages/drag-canvas/src/lib/drag-canvas.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect, memo } from 'react'
 import { DndContext, useDraggable, useDndContext } from '@dnd-kit/core'
-import { CSS } from '@dnd-kit/utilities'
+import { CSS, Transform } from '@dnd-kit/utilities'
 import { restrictToParentElement } from '@dnd-kit/modifiers'
 
 import classNames from 'classnames'
@@ -25,6 +25,7 @@ interface DraggableItemProps {
   canvasWidth: number
   canvasHeight: number
   initialPosition?: Position
+  snapTolerance?: number
 }
 
 interface MouseDownClickPosition {
@@ -52,25 +53,157 @@ interface ResizeLimit {
 const handles = ['tl', 'tm', 'tr', 'mr', 'br', 'bm', 'bl', 'ml']
 
 const DraggableItem: React.FC<DraggableItemProps> = (props) => {
-  const { id, maxWidth, maxHeight, canvasWidth, canvasHeight, initialPosition } = props
-  const minWidth = props.minWidth || 10
-  const minHeight = props.minHeight || 10
+  const { id, maxWidth = 10, maxHeight = 10, canvasWidth, canvasHeight, initialPosition, snapTolerance = 5 } = props
 
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id,
   })
+  const context = useDndContext()
 
   const [width, setWidth] = useState(150)
   const [height, setHeight] = useState(150)
 
   const [position, setPosition] = useState(initialPosition || { x: 0, y: 0 })
 
+  const transformRef = useRef<Transform>(null)
+
+  function snapCheck() {
+    if (!transformRef.current) return
+    const x = position.x + transformRef.current.x
+    const y = position.y + transformRef.current.y
+    let activeLeft = x
+    let activeRight = x + width
+    let activeTop = y
+    let activeBottom = y + height
+
+    // 初始化辅助线数据
+    const temArr = () => new Array(3).fill({ display: false, position: '', origin: '', lineLength: '' })
+    const refLine: Record<string, any> = { vLine: temArr(), hLine: temArr() }
+
+    // 获取所有节点
+    const { draggableNodes, active } = context
+
+    // 除了本节点
+    const nodes = [...draggableNodes.values()].filter((node) => active?.id !== node?.id)
+
+    let tem: {
+      value: any
+      display: boolean[]
+      position: number[]
+    } = {
+      value: { x: [[], [], []], y: [[], [], []] },
+      display: [],
+      position: [],
+    }
+
+    for (let item of nodes) {
+      const dom = item?.node.current as HTMLElement
+      const w = +dom.style.width.replace(/px/g, '')
+      const h = +dom.style.height.replace(/px/g, '')
+      const l = +dom.style.left.replace(/px/g, '')
+      const t = +dom.style.top.replace(/px/g, '')
+
+      const r = l + w // 对齐目标right
+      const b = t + h // 对齐目标的bottom
+
+      const hc = Math.abs(activeTop + height / 2 - (t + h / 2)) <= snapTolerance // 水平中线
+      const vc = Math.abs(activeLeft + width / 2 - (l + w / 2)) <= snapTolerance // 垂直中线
+
+      const ts = Math.abs(t - activeBottom) <= snapTolerance // 从上到下
+      const TS = Math.abs(b - activeBottom) <= snapTolerance // 从上到下
+      const bs = Math.abs(t - activeTop) <= snapTolerance // 从下到上
+      const BS = Math.abs(b - activeTop) <= snapTolerance // 从下到上
+
+      const ls = Math.abs(l - activeRight) <= snapTolerance // 外左
+      const LS = Math.abs(r - activeRight) <= snapTolerance // 外左
+      const rs = Math.abs(l - activeLeft) <= snapTolerance // 外右
+      const RS = Math.abs(r - activeLeft) <= snapTolerance // 外右
+
+      tem['display'] = [ts, TS, bs, BS, hc, hc, ls, LS, rs, RS, vc, vc]
+      tem['position'] = [t, b, t, b, t + h / 2, t + h / 2, l, r, l, r, l + w / 2, l + w / 2]
+
+      // fix：中线自动对齐，元素可能超过父元素边界的问题
+
+      let snapX = null
+      let snapY = null
+      if (ts) {
+        snapY = t - height
+        console.log('t - height: ', t - height)
+
+        tem.value.y[0].push(l, r, activeLeft, activeRight)
+      }
+      if (bs) {
+        snapY = t
+        console.log('t: ', t)
+        tem.value.y[0].push(l, r, activeLeft, activeRight)
+      }
+      if (TS) {
+        snapY = b - height
+
+        tem.value.y[1].push(l, r, activeLeft, activeRight)
+      }
+      if (BS) {
+        snapY = b
+
+        tem.value.y[1].push(l, r, activeLeft, activeRight)
+      }
+
+      if (ls) {
+        snapX = l - width
+
+        tem.value.x[0].push(t, b, activeTop, activeBottom)
+      }
+      if (rs) {
+        snapX = l
+
+        tem.value.x[0].push(t, b, activeTop, activeBottom)
+      }
+      if (LS) {
+        snapX = r - width
+
+        tem.value.x[1].push(t, b, activeTop, activeBottom)
+      }
+      if (RS) {
+        snapX = r
+        tem.value.x[1].push(t, b, activeTop, activeBottom)
+      }
+
+      if (hc) {
+        snapY = t + h / 2 - height / 2
+
+        tem.value.y[2].push(l, r, activeLeft, activeRight)
+      }
+      if (vc) {
+        snapX = l + w / 2 - width / 2
+        tem.value.x[2].push(t, b, activeTop, activeBottom)
+      }
+
+      // 设置吸附
+      if (transformRef.current && snapX !== null) {
+        transformRef.current.x = snapX - position.x
+      }
+      if (transformRef.current && snapY !== null) {
+        transformRef.current = {
+          ...transformRef.current,
+          y: snapY - position.y,
+        }
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (transform !== null) {
+      transformRef.current = transform
+      snapCheck()
+    }
+  }, [transform])
+
   // 监听事件
   useEffect(() => {
-    const cb = (data: { delta: { x: number; y: number } }) => {
+    const cb = () => {
       setPosition((prevPositions) => ({
-        x: prevPositions.x + data.delta.x,
-        y: prevPositions.y + data.delta.y,
+        x: prevPositions.x + (transformRef.current as Transform).x,
+        y: prevPositions.y + (transformRef.current as Transform).y,
       }))
     }
     eventBus.on(`${id}-dragEnd`, cb)
@@ -89,12 +222,12 @@ const DraggableItem: React.FC<DraggableItemProps> = (props) => {
     position: 'absolute',
     top: position.y,
     left: position.x,
-    transform: transform ? CSS.Translate.toString(transform) : undefined,
+    transform: transform ? CSS.Translate.toString(transformRef.current) : undefined,
     // transition: transform ? 'transform 0.1s ease' : undefined,
     width: width,
     height: height,
     border: '1px solid black',
-    backgroundColor: id === 'item1' ? 'red' : 'green',
+    backgroundColor: id === 'item1' ? '#ddd' : '#aaa',
   }
 
   const handleOnMouseDown = (handle: string, event: React.MouseEvent<HTMLDivElement>) => {
