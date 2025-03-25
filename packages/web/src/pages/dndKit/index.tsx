f4ebc7f7258b3fd33418939b709206bf9b514964
lizheng <lizheng@tencent.com>
2025-03-25 22:56:30 +0800

    feat: 增加吸附线

--- packages/web/src/pages/dndKit/index.tsx
+++ packages/web/src/pages/dndKit/index.tsx
@@ -6,10 +6,28 @@ const DndKit = () => {
   return (
     <div>
       <DragCanvas width={1000} height={800}>
-        <DraggableItem id="item1" initialPosition={{ x: 0, y: 0 }} minWidth={100} minHeight={100}>
+        <DraggableItem
+          id="item1"
+          initialPosition={{ x: 0, y: 0 }}
+          minWidth={100}
+          minHeight={100}
+          initialSize={{
+            width: 100,
+            height: 100,
+          }}
+        >
           item1
         </DraggableItem>
-        <DraggableItem id="item2" initialPosition={{ x: 100, y: 100 }} minWidth={100} minHeight={100}>
+        <DraggableItem
+          id="item2"
+          initialPosition={{ x: 500, y: 500 }}
+          minWidth={100}
+          minHeight={100}
+          initialSize={{
+            width: 200,
+            height: 200,
+          }}
+        >
           item2
         </DraggableItem>
       </DragCanvas>
